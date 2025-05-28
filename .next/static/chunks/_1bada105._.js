d662eb553c93b0a4f3bb57ccbcf09f4536000e9b
Cüneyt Yetgin <cuneyt.yetgin@gmail.com>
2025-05-28 17:29:40 +0000

    replace tariff text with the following: Tariff is the digital customs regulations databank  solution for users and systems to reach historically indexed customs duties, taxes and measures, applied in accordance with Tariff Schedules used across customs territories. Tariff currently covers the European Union, United Kingdom, Switzerland, and Türkiye regulations and provides customs regulations, duties, and control measures with just a few clicks, Tariff enables users to access critical information, tailored to their trade routes, ensuring a smoother, more efficient, and compliant import or export readiness.

--- .next/static/chunks/_1bada105._.js
+++ .next/static/chunks/_1bada105._.js
@@ -224,8 +224,8 @@ function TariffPage() {
                 children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
                     src: "https://firebasestorage.googleapis.com/v0/b/global-hub-21v8j.firebasestorage.app/o/TARIFF%402x-v2.png?alt=media&token=a94dcd84-fe64-4699-9ea2-70ec737ca484",
                     alt: t('products.tariff.pageTitle'),
-                    width: 310,
-                    height: 90,
+                    width: 425,
+                    height: 135,
                     priority: true
                 }, void 0, false, {
                     fileName: "[project]/src/app/products/tariff/page.tsx",
